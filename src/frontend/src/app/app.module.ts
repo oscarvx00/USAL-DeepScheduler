51051389b2f2b9499fbececa226edbd34f5d3e36
oscarvx00 <oscarvicente@usal.es>
2022-02-19 14:13:00 +0100

    Working on register Previous commit before setting scss as default

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card'; 
+
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { MainComponent } from './components/main/main.component';
@@ -26,6 +28,7 @@ import { MainComponent } from './components/main/main.component';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
