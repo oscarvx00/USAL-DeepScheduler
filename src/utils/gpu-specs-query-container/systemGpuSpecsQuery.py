3754831d5baa55ff52de8c9e00c609d69a5e9733
oscarvx00 <oscarvicente@usal.es>
2022-05-08 19:25:09 +0200

    gpu spectr query renamed

--- src/utils/gpu-specs-query-container/systemGpuSpecsQuery.py
+++ src/utils/gpu-specs-query-container/systemGpuSpecsQuery.py
@@ -1,13 +1,11 @@
 import json
-import re
 import subprocess
 
-def getProps():
+def getNvidiaProps():
     props = {
         'gpu_name' : getProp('name'),
         'driver_version' : getProp('driver_version'),
-        'compute_cap' : getProp('compute_cap'),
-        #'node_ip' : getIp()
+        'compute_cap' : getProp('compute_cap')
     }
     return props
 
@@ -23,8 +21,4 @@ def getProp(prop):
     except:
          return None
 
-def getIp():
-    res = subprocess.check_output(['ip', 'route', 'get', '8.8.8.8'])
-    return res
-
-print(json.dumps(getProps()))
+print(json.dumps(getNvidiaProps()))
--- src/utils/node-specs-query-container/buildPush.sh
+++
@@ -1,2 +0,0 @@
-docker build -t oscarvicente/node-specs-query .
-docker push oscarvicente/node-specs-query
